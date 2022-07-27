e91c8a10496d157471a3b819bc65b1200ca7d7d9
Vicente Pareja <vicente.pareja@uc.cl>
2022-07-26 20:49:46 -0400

    función main

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ pygame.init()
 SIZE = 800, 600
 window = pygame.display.set_mode(SIZE)
 
-run = True
-while run:
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            print(event == pygame.QUIT)
-            run = False
 
-pygame.quit()
+def main():
+    run = True
+    #Es un loop que corre el juego
+    while run:
+        #Chequea los eventos
+        for event in pygame.event.get():
+
+            #Si se apreta la X se acaba el loop del juego
+            if event.type == pygame.QUIT:
+                print(event == pygame.QUIT)
+                run = False
+
+    pygame.quit()
+
+main()
